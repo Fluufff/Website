c7981021007bc1c0c0bd812ab290ac9de9141aa4
Glenn Dirkx <glenn.dirkx@gmail.com>
2026-04-30 09:04:52 +0200

    migrate css some more

--- src/components/countdown.vue
+++ src/components/countdown.vue
@@ -1,34 +1,55 @@
 <template>
   <div class="registration_banner_countdown">
     <div>
-      <span class="time">{{ String(days).padStart(2, '0') }}</span>
+      <span class="time">{{ padded(time.days) }}</span>
       <span class="label">Days</span>
     </div>
     :
     <div>
-      <span class="time">{{ String(hours).padStart(2, '0') }}</span>
+      <span class="time">{{ padded(time.hours) }}</span>
       <span class="label">Hours</span>
     </div>
     :
     <div>
-      <span class="time">{{ String(minutes).padStart(2, '0') }}</span>
+      <span class="time">{{ padded(time.minutes) }}</span>
       <span class="label">Minutes</span>
     </div>
     :
     <div>
-      <span class="time">{{ String(seconds).padStart(2, '0') }}</span>
+      <span class="time">{{ padded(time.seconds) }}</span>
       <span class="label">Seconds</span>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-div {
-  position: relative;
+@use '../styles/charter';
 
-  .label {
-    position: absolute;
-    bottom: -12px;
+.registration_banner_countdown {
+  display: flex;
+  flex-direction: row;
+  gap: 2px;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 2em;
+
+    position: relative;
+
+    .label {
+      color: charter.$accent1-300;
+      font-family: var(--font-inter), sans-serif;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 500;
+      text-transform: uppercase;
+      line-height: 12px;
+
+      position: absolute;
+      bottom: -12px;
+    }
   }
 }
 </style>
@@ -43,10 +64,16 @@ const props = defineProps({
   }
 })
 
-const days = ref(0)
-const hours = ref(0)
-const minutes = ref(0)
-const seconds = ref(0)
+const time = ref({
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0
+})
+
+const padded = (t: number) => {
+  return String(t).padStart(2, '0')
+}
 
 let interval: NodeJS.Timeout
 updateCountdown()
@@ -57,15 +84,15 @@ function updateCountdown() {
   const diff = target - now
 
   if (diff <= 0) {
-    days.value = hours.value = minutes.value = seconds.value = 0
+    time.value.days = time.value.hours = time.value.minutes = time.value.seconds = 0
     clearInterval(interval)
     return
   }
 
-  days.value = Math.floor(diff / (1000 * 60 * 60 * 24))
-  hours.value = Math.floor((diff / (1000 * 60 * 60)) % 24)
-  minutes.value = Math.floor((diff / (1000 * 60)) % 60)
-  seconds.value = Math.floor((diff / 1000) % 60)
+  time.value.days = Math.floor(diff / (1000 * 60 * 60 * 24))
+  time.value.hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
+  time.value.minutes = Math.floor((diff / (1000 * 60)) % 60)
+  time.value.seconds = Math.floor((diff / 1000) % 60)
 }
 
 onMounted(() => {
