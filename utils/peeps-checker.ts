ae04adc27dd6b793f4f14a58c6d46310ba9d85e4
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-05-20 12:26:46 +0200

    Convert script to read the chip contents

--- utils/peeps-checker.ts
+++ utils/peeps-checker.ts
@@ -2,6 +2,8 @@
 
 // deno run --allow-env --env-file=.env.local --allow-net --allow-read utils/peeps-checker.ts
 
+import assert from 'node:assert';
+
 const { PEEPS_CLIENT_ID, PEEPS_CLIENT_SECRET, PEEPS_REFRESH_TOKEN, PEEPS_SPREADSHEET_ID } = Deno.env.toObject()
 
 // get access token from refresh token
@@ -17,30 +19,34 @@ const access_token_response = await fetch('https://oauth2.googleapis.com/token',
 const access_token_json = await access_token_response.json()
 const access_token = access_token_json['access_token']
 
-// find the name of the first sheet
-const spreadsheet_meta_response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${PEEPS_SPREADSHEET_ID}`, {
+// retrieve the volunteers list
+const spreadsheet_response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${PEEPS_SPREADSHEET_ID}?` + new URLSearchParams({
+  includeGridData: 'true',
+  fields: 'sheets.data.rowData.values(formattedValue,chipRuns)',
+}), {
   headers: {
     Authorization: `Bearer ${access_token}`
   }
 })
-const spreadsheet_meta_json = await spreadsheet_meta_response.json()
-const sheet_title = spreadsheet_meta_json['sheets'][0]['properties']['title']
+const spreadsheet_json = await spreadsheet_response.json()
+const spreadsheet_rows = spreadsheet_json['sheets'][0]['data'][0]['rowData']
 
-// get the contents of the first sheet
-const speadsheet_values_response = await fetch(
-  `https://sheets.googleapis.com/v4/spreadsheets/${PEEPS_SPREADSHEET_ID}/values:batchGet?ranges=${encodeURI(sheet_title)}`,
-  {
-    headers: {
-      Authorization: `Bearer ${access_token}`
-    }
+// this script operates under the assumption that the first sheet contains these columns
+const spreadsheet_header = spreadsheet_rows.shift()['values'].map((row: Record<string, string>) => row['formattedValue'])
+assert.equal(spreadsheet_header.join(', '), 'Team member, Department, Role, Notes')
+
+
+// deno-lint-ignore no-explicit-any
+let all_volunteers: Record<string, string>[] = spreadsheet_rows.map((spreadsheet_row: any) => {
+  return {
+    id: spreadsheet_row['values'][0]['chipRuns'][0]['chip']['personProperties']['email'],
+    name: spreadsheet_row['values'][0]['formattedValue'],
+    department: spreadsheet_row['values'][1]['formattedValue'],
+    role: spreadsheet_row['values'][2]?.['formattedValue'],
   }
-)
-const spreadsheet_values_json = await speadsheet_values_response.json()
-const spreadsheat_values_rows = spreadsheet_values_json['valueRanges'][0]['values']
+})
 
-// resume legacy code
-let all_volunteers = spreadsheat_values_rows
-all_volunteers = all_volunteers.filter((row: string[]) => ['Head', 'Deputy'].includes(row[2])) // role filter
+all_volunteers = all_volunteers.filter(volunteer => ['Head', 'Deputy'].includes(volunteer.role)) // role filter
 
 const about = Deno.readTextFileSync('./src/pages/about.astro')
 const peeps = Array.from(about.matchAll(/Peep name="(.*)" title=["{](.*?)[}"]/g)).map((result) => [
@@ -60,16 +66,16 @@ peeps.forEach((peep) => {
 const volunteer_name_to_peep_map = new Map()
 
 peeps.forEach((peep: string[]) => {
-  const volunteers = all_volunteers.filter((volunteer: string[]) => volunteer[0].includes(peep[0]))
+  const volunteers = all_volunteers.filter(volunteer => volunteer['name'].includes(peep[0]))
   if (volunteers.length == 0) {
     console.log(`peep "${peep[0]}" (${peep[1]}) seems to be a regular volunteer.`)
   } else {
-    volunteer_name_to_peep_map.set(volunteers[0][0], peep)
+    volunteer_name_to_peep_map.set(volunteers[0]['name'], peep)
 
     for (const role of peep[1]) {
       if (role == 'Chairman') continue
-      const volunteer = volunteers.find((volunteer: string[]) => {
-        return role == `${volunteer[2]} of ${volunteer[1]}`
+      const volunteer = volunteers.find(volunteer => {
+        return role == `${volunteer['role']} of ${volunteer['department']}`
       })
       if (!volunteer) {
         console.log(`peep "${peep[0]}" seems to no longer hold the "${role}" role.`)
@@ -78,14 +84,14 @@ peeps.forEach((peep: string[]) => {
   }
 })
 
-all_volunteers.forEach((volunteer: string[]) => {
-  const peep = volunteer_name_to_peep_map.get(volunteer[0])
-  const role = `${volunteer[2]} of ${volunteer[1]}`
+all_volunteers.forEach(volunteer => {
+  const peep = volunteer_name_to_peep_map.get(volunteer['name'])
+  const role = `${volunteer['role']} of ${volunteer['department']}`
   if (peep == undefined) {
-    console.log(`volunteer "${volunteer[0]}" lacks a peep for their "${role}" role.`)
+    console.log(`volunteer "${volunteer['name']}" lacks a peep for their "${role}" role.`)
   } else {
     if (!peep[1].includes(role)) {
-      console.log(`volunteer "${volunteer[0]}" lacks a mention of their "${role}" role.`)
+      console.log(`volunteer "${volunteer['name']}" lacks a mention of their "${role}" role.`)
     }
   }
 })
