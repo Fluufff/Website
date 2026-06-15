4821e2fcc2b344ff56876ec81063d723d8543149
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-06-15 10:49:53 +0200

    Revert "Add the schedule open locations to fess.json"
    This reverts commit 725e39bff05c100b909b569f4c95e46fff57ac96.

--- src/pages/api/v1/fess.json.ts
+++ src/pages/api/v1/fess.json.ts
@@ -10,7 +10,6 @@ import { start_con } from '../../../data/reg/dates.json'
 const raw_tags = await getCollection('scheduleTags')
 const raw_locations = await getCollection('scheduleLocations')
 const raw_events = await getCollection('scheduleEvents')
-const raw_open_locations = await getCollection('scheduleOpenLocations')
 
 const timezone = 'Europe/Brussels'
 const offset = start_con.split('+')[1] // assumes the con happens entirely in the same offset
@@ -85,40 +84,22 @@ export const GET: APIRoute = () => {
         displayName: raw_host.data.name
       }
     }),
-    sessions: [
-      ...raw_events.map((raw_event: any) => {
-        return {
-          id: id(raw_event.id),
-          displayName: en_US(raw_event.data.title),
-          description: en_US(raw_event.data.description),
-          timeSlots: [
-            {
-              startTime: `${raw_event.data.day}T${raw_event.data.start_time}+${offset}`,
-              endTime: `${raw_event.data.day}T${raw_event.data.end_time}+${offset}`,
-              roomIds: raw_event.data.schedule_location ? [id(raw_event.data.schedule_location.id)] : [],
-              hostIds: raw_event.data.host_name ? [get_host_id_from_host_name(raw_event.data.host_name)] : [],
-              labelIds: raw_event.data.schedule_tags.map((schedule_tag: any) => id(schedule_tag.id))
-            }
-          ]
-        }
-      }),
-      ...raw_open_locations.map((raw_open_location: any) => {
-        return {
-          id: raw_open_location.id, // conveniently already not numeric so it will not collide with event ids
-          displayName: en_US(raw_open_location.data.name),
-          description: en_US(`placeholder text for the ${raw_open_location.data.name.toLowerCase()}.`),
-          timeSlots: raw_open_location.data.opening_times.map((opening_time: any) => {
-            return {
-              startTime: `${opening_time.day}T${opening_time.start_time}+${offset}`,
-              endTime: `${opening_time.day}T${opening_time.end_time}+${offset}`,
-              roomIds: [],
-              hostIds: [get_host_id_from_host_name('Flüüfff')],
-              labelIds: []
-            }
-          })
-        }
-      })
-    ]
+    sessions: raw_events.map((raw_event: any) => {
+      return {
+        id: id(raw_event.id),
+        displayName: en_US(raw_event.data.title),
+        description: en_US(raw_event.data.description),
+        timeSlots: [
+          {
+            startTime: `${raw_event.data.day}T${raw_event.data.start_time}+${offset}`,
+            endTime: `${raw_event.data.day}T${raw_event.data.end_time}+${offset}`,
+            roomIds: raw_event.data.schedule_location ? [id(raw_event.data.schedule_location.id)] : [],
+            hostIds: raw_event.data.host_name ? [get_host_id_from_host_name(raw_event.data.host_name)] : [],
+            labelIds: raw_event.data.schedule_tags.map((schedule_tag: any) => id(schedule_tag.id))
+          }
+        ]
+      }
+    })
   }
 
   return new Response(JSON.stringify(data), {
