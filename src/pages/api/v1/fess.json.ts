b78ce3d2b1899e60d5ea58942b4c6b04eaa35f3d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-06-15 10:50:16 +0200

    Add the schedule open locations to fess.json more cleanly

--- src/pages/api/v1/fess.json.ts
+++ src/pages/api/v1/fess.json.ts
@@ -10,6 +10,7 @@ import { start_con } from '../../../data/reg/dates.json'
 const raw_tags = await getCollection('scheduleTags')
 const raw_locations = await getCollection('scheduleLocations')
 const raw_events = await getCollection('scheduleEvents')
+const raw_open_locations = await getCollection('scheduleOpenLocations')
 
 const timezone = 'Europe/Brussels'
 const offset = start_con.split('+')[1] // assumes the con happens entirely in the same offset
@@ -101,6 +102,23 @@ export const GET: APIRoute = () => {
       }
     })
   }
+
+  raw_open_locations.forEach((raw_open_location: any) => {
+    data.sessions.push({
+      id: raw_open_location.id, // conveniently already not numeric so it will not collide with event ids
+      displayName: en_US(raw_open_location.data.name),
+      description: en_US(`Flüüfff placeholder text for the ${raw_open_location.data.name.toLowerCase()}.`),
+      timeSlots: raw_open_location.data.opening_times.map((opening_time: any) => {
+        return {
+          startTime: `${opening_time.day}T${opening_time.start_time}+${offset}`,
+          endTime: `${opening_time.day}T${opening_time.end_time}+${offset}`,
+          roomIds: [],
+          hostIds: [get_host_id_from_host_name('Flüüfff')],
+          labelIds: []
+        }
+      })
+    })
+  })
 
   return new Response(JSON.stringify(data), {
     headers: {
