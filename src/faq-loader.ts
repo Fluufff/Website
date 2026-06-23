1e3449fa6012aa8ee81e86c47f7d70ac9f2bffd2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-06-23 21:29:47 +0200

    Move faq content to the cms

---
+++ src/faq-loader.ts
@@ -0,0 +1,59 @@
+import type { Loader, LoaderContext } from 'astro/loaders'
+import { z } from 'astro:content'
+import type { Strapi } from './strapi.ts'
+
+// export function faqEntryLoader(strapi: Strapi): Loader {
+//   return {
+//     name: 'faq-entry-loader',
+//     async load(ctx: LoaderContext) {
+//       ctx.store.clear()
+
+//       const items = await strapi.fetchItems('faq-entries')
+
+//       for (const item of items) {
+//         const data = await ctx.parseData({
+//           id: item.id.toString(),
+//           data: { ...item }
+//         })
+
+//         ctx.store.set({ id: item.id.toString(), data })
+//       }
+//     },
+//     schema: z.object({
+//       id: z.number(),
+//       question: z.string(),
+//       answer: z.string()
+//     })
+//   }
+// }
+
+export function faqPageLoader(strapi: Strapi): Loader {
+  return {
+    name: 'faq-page-loader',
+    async load(ctx: LoaderContext) {
+      ctx.store.clear()
+
+      const items = await strapi.fetchItems('faq-pages', { populate: 'faq_entries' })
+
+      for (const item of items) {
+        const data = await ctx.parseData({
+          id: item.id.toString(),
+          data: { ...item }
+        })
+
+        ctx.store.set({ id: item.id.toString(), data })
+      }
+    },
+    schema: z.object({
+      id: z.number(),
+      name: z.string(),
+      faq_entries: z.array(
+        z.object({
+          id: z.number(),
+          question: z.string(),
+          answer: z.string()
+        })
+      )
+    })
+  }
+}
