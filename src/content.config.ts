c39e90127c22aa5b1d4bcf3188f8962fee57960b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-06-23 21:32:16 +0200

    Faq from cms (#136)
    * Move faq content to the cms
    
    * Formatting
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,10 @@ import {
   scheduleLocationLoader,
   scheduleEventLoader
 } from './schedule-loader.ts'
+import {
+  // faqEntryLoader,
+  faqPageLoader
+} from './faq-loader.ts'
 import { loadStrapi } from './strapi.ts'
 
 const strapi = loadStrapi()
@@ -15,5 +19,7 @@ export const collections = {
   scheduleOpenLocations: defineCollection({ loader: scheduleOpenLocationLoader(strapi) }),
   scheduleLocations: defineCollection({ loader: scheduleLocationLoader(strapi) }),
   scheduleEvents: defineCollection({ loader: scheduleEventLoader(strapi) }),
-  scheduleTags: defineCollection({ loader: scheduleTagLoader(strapi) })
+  scheduleTags: defineCollection({ loader: scheduleTagLoader(strapi) }),
+  // faqEntries: defineCollection({ loader: faqEntryLoader(strapi) }),
+  faqPages: defineCollection({ loader: faqPageLoader(strapi) })
 }
