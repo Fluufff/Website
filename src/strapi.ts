a043f6a3e6c4ff9fa4159f332a9d45772694d303
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-06-23 23:47:59 +0200

    Stash failed attempt

--- src/strapi.ts
+++ src/strapi.ts
@@ -3,6 +3,7 @@ import qs from 'qs'
 import process from 'node:process'
 
 export interface Strapi {
+  warnings: string[][],
   // deno-lint-ignore no-explicit-any
   fetchItems(itemName: string, options?: object): Promise<any[]> // eslint-disable-line @typescript-eslint/no-explicit-any
 }
@@ -25,7 +26,9 @@ export function loadStrapi(): Strapi {
     if (!isDevelopment) throw new Error('Missing STRAPI_URI or STRAPI_TOKEN environment variables')
   }
 
+  const warnings: string[][] = []
   return {
+    warnings,
     fetchItems: async (itemName: string, options?: object) => {
       try {
         const data = await fetchFromStrapi(STRAPI_URI!, STRAPI_TOKEN!, itemName, options)
@@ -33,7 +36,9 @@ export function loadStrapi(): Strapi {
         return data
       } catch (e) {
         const msg = e instanceof Error ? e.message : e
-        console.warn(`Error while trying to fetch ${itemName}:`, msg)
+        const warning: string[] = [`Error while trying to fetch ${itemName}:`, msg as string]
+        console.warn(...warning)
+        warnings.push(warning)
         return []
       }
     }
@@ -42,6 +47,7 @@ export function loadStrapi(): Strapi {
 
 function dummyStrapi(): Strapi {
   return {
+    warnings: [['running in dummy mode.']],
     fetchItems: () => Promise.resolve([])
   }
 }
