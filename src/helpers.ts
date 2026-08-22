27f4ca22a10b1555a60f8370eea461577b660d01
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-08-22 12:12:25 +0200

    Fix markdown newline preserver breaking code blocks

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,5 @@
 import { marked } from 'marked'
 
 export function render_markdown(markdown: string): string {
-  return marked.parse(markdown.replaceAll('\n', '<br />'), { breaks: true }) as string
+  return marked.parse(markdown.replace(/^$/gm, '<br />\n'), { breaks: true }) as string
 }
